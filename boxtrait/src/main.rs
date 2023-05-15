797924b1c336316844f7cbb191c0b2aab2c6735b
rafiramadhana <rf.ramadhana@gmail.com>
2023-05-15 08:19:11 +0700

    Update box trait
    Add example related to burrowing and struct.

--- boxtrait/src/main.rs
+++ boxtrait/src/main.rs
@@ -1,3 +1,23 @@
+struct List {
+    // Error because compiler does not know the size of List
+    // item: Option<List>,
+
+    // OK
+    item: Option<Box<List>>,
+}
+
+fn drop_me<T>(_: T) {}
+
 fn main() {
-    println!("Hello, world!");
+    let num = 1;
+    drop_me(num);
+    drop_me(num);
+
+    let bx = Box::new(1);
+    drop_me(bx.clone());
+    // drop_me(bx);
+    println!("{:?}", bx);
+    println!("{:?}", *bx); // Box can also be "dereferenced"
+
+    // TODO: Add "Box around traits"
 }
